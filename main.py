5983809c43ba746f5a3b4e60828d8c0245d5840b
Ashish Kumar <ashishrj08mehta@gmail.com>
2025-09-24 12:46:08 +0530

    path added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
+
 app=FastAPI()
 
 #load data
@@ -7,8 +8,6 @@ def load_data():
     with open("patient.json" , "r") as f:
         data=json.load(f)
     return data
-
-
 
 @app.get("/")
 def patient_page():
@@ -18,3 +17,15 @@ def patient_page():
 def about():
     return {"It is API for patient management"}
 
+@app.get("/view")
+def view():
+    data=load_data()
+    return data
+
+# path parameter
+@app.get("/patient_view/{patient_id}")
+def patient_view(patient_id:str = Path(...,description='It is ID of patient',example='P001')):
+    data=load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return {"error":"patient not found"}
